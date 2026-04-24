8f3cdccb80ec36710c797af5296152f00b349379
percussionfloh <florian.moldaschl@web.de>
2026-04-24 20:56:04 +0200

    setup nuxt/content and nuxtjs/supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
+import { execSync } from 'node:child_process';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
+    modules: ['@nuxt/content', '@nuxtjs/supabase'],
+    css: ['~/assets/main.css'],
+});
+
