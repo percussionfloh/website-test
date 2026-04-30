f48b4114d345b43626ddfe22715350d7fc383fec
percussionfloh <florian.moldaschl@web.de>
2026-04-30 22:26:49 +0200

    fix citation.data.data.xyz in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,30 +1,32 @@
 <script setup>
 const client = useSupabaseClient()
 const citations = ref([])
-const citationName = ref('')
+// const citationName = ref('')
 
-const createCitation = async () => {
-  const { data, error } = await client.from('citations').insert({
-    name: citationName.value
-  }).select().single()
-  citations.value.push(data)
-  if (error) throw error
-  citationName.value = ''
-}
+// const createCitation = async () => {
+//   const { data, error } = await client.from('citations').insert({
+//     name: citationName.value
+//   }).select().single()
+//   citations.value.push(data)
+//   if (error) throw error
+//   citationName.value = ''
+// }
 
 onMounted(async ()=> {
   const { data, error } = await client.from('citations').select()
   citations.value = data
+
+console.log(citations.value[0])
 })
+
+
 </script>
-
-
 
 <template>
   <div class="min-h-screen bg-slate-900 text-white p-5">
-    <h1 class="text-4xl font-bold text-cyan-400">Citations</h1>
+    <h1 class="text-4xl font-bold text-cyan-400">ss</h1>
     <ul class="list-disc ml-5 text-xl mt-4">
-        <li v-for="citation in citations" :key="citation.id">123{{ citation.id }}</li>
+        <li v-for="citation in citations" :key="citation.key">123{{ citation.data.data.title }}</li>
     </ul>
   </div>
 </template>
